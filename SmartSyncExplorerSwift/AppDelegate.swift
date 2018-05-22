4c99e15a4ecf4f86a8068faed5e6bfb2167010ec
Raj Rao <rao.r@salesforce.com>
2018-05-22 12:41:52 -0700

    Assets and Views

--- SmartSyncExplorerSwift/AppDelegate.swift
+++ SmartSyncExplorerSwift/AppDelegate.swift
@@ -147,7 +147,7 @@ class AppDelegate : UIResponder, UIApplicationDelegate
     
     func setupRootViewController()
     {
-        let rootVC = RootViewController(nibName: nil, bundle: nil)
+        let rootVC = RootViewController()
         let navVC = UINavigationController(rootViewController: rootVC)
         self.window!.rootViewController = navVC
     }
